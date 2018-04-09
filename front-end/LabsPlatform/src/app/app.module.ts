56a1a14fed8e4f7da6eac00047a6ce8d35d3e282
Dimmy Marcelissen <dimwes@hotmail.com>
2018-04-09 11:02:01 +0200

    Vernieuwde home page

--- front-end/LabsPlatform/src/app/app.module.ts
+++ front-end/LabsPlatform/src/app/app.module.ts
@@ -1,31 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule} from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { CookieService } from 'ngx-cookie-service';
 
 import { AppComponent } from './app.component';
-import { BadgeSelectComponent } from './badge-select-screen/badge-select-screen.component';
-import { BadgeclassService } from './services/badgeclass.service';
-import { AssertionService } from './services/assertion.service';
-import { IssuerService } from './services/issuer.service';
-import { ROUTER } from '../routes';
+import {HeaderTopComponent} from "./header/header-top.component";
+import {MainContentComponent} from "./main-content/main-content.component";
+import {AccountComponent} from "./main-content/account/account.component";
+import {MyBadgesComponent} from "./main-content/mybadges/mybadges.component";
+import {MyAccountComponent} from "./main-content/account/myaccount/myaccount.component";
 
 
 @NgModule({
   declarations: [
+    AccountComponent,
     AppComponent,
-    BadgeSelectComponent
+    HeaderTopComponent,
+    MainContentComponent,
+    MyAccountComponent,
+    MyBadgesComponent,
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(ROUTER)
+    BrowserModule
   ],
-  providers: [BadgeclassService,AssertionService,IssuerService, CookieService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front-end/LabsPlatform/src/app/header/header-top.component.ts
@@ -0,0 +1,12 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'header-top',
+    templateUrl: 'header-top.component.html',
+    styleUrls: ['header-top.component.css']
+})
+
+export class HeaderTopComponent {
+
+}
