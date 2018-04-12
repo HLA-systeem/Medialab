c14d5bf44e5de91129e6ecf10e8570f836ac4d68
Dimmy Marcelissen <dimwes@hotmail.com>
2018-04-12 11:12:03 +0200

    Add workshop component

---
+++ front-end/LabsPlatform/src/app/workshop-add-component/workshop-add-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkshopAddComponentComponent } from './workshop-add-component.component';
+
+describe('WorkshopAddComponentComponent', () => {
+  let component: WorkshopAddComponentComponent;
+  let fixture: ComponentFixture<WorkshopAddComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WorkshopAddComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkshopAddComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
